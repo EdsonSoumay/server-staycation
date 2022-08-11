1f25757f7c2f415dbfd2c1fe703b86eeb1eb17fe
EdsonSoumay <69837656+EdsonSoumay@users.noreply.github.com>
2022-08-12 07:10:21 +0900

    Update app.js
    add cors again (9999)

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res, next)=>{
   res.setHeader('Access-Control-Allow-Methods','GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS') // method = method dalam penggunaan API 
   //res.setHeader('Access-Control-Allow-Headers','Content-Type', 'Authorization') // Content-Type = contohnya json, (xml, html?) dll. // Authorization = berguna ketika proses pengiriman token kedalam API
  //res.setHeader('Access-Control-Allow-Headers','Content-Type', 'Authorization') // Content-Type = contohnya json, (xml, html?) dll. // Authorization = berguna ketika proses pengiriman token kedalam API
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+ res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
  next(); // agar requestnya tidak berhenti sampai disitu
 })
 
