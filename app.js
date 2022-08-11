b44525d29e8027b15b4c88b1cca266032488f270
EdsonSoumay <69837656+EdsonSoumay@users.noreply.github.com>
2022-08-12 07:43:40 +0900

    Update app.js

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ var app = express();
 //  next(); // agar requestnya tidak berhenti sampai disitu
 // })
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,PUT,DELETE,OPTIONS");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,PUT,DELETE,OPTIONS");
+//   next();
+// });
 
 
 //import mongoose
