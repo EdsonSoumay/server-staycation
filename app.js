5bf5ca4535e896cdca2aa2d6fc63a7cdb7d121db
EdsonSoumay <69837656+EdsonSoumay@users.noreply.github.com>
2022-08-12 07:02:05 +0900

    Update app.js
    update cors(4)

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ var app = express();
 // untuk handle siapa sja yang dapat mengakses API kita
 app.use((req, res, next)=>{
   res.setHeader('Access-Control-Allow-Origin','*') // Origin = url yang ingin di berikan akses API 
-  res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE, OPTIONS') // method = method dalam penggunaan API 
-  res.setHeader('Access-Control-Allow-Headers','Content-Type', 'Authorization') // Content-Type = contohnya json, (xml, html?) dll. // Authorization = berguna ketika proses pengiriman token kedalam API
-  next(); // agar requestnya tidak berhenti sampai disitu
+  res.setHeader('Access-Control-Allow-Methods','GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS') // method = method dalam penggunaan API 
+  //res.setHeader('Access-Control-Allow-Headers','Content-Type', 'Authorization') // Content-Type = contohnya json, (xml, html?) dll. // Authorization = berguna ketika proses pengiriman token kedalam API
+ //res.setHeader('Access-Control-Allow-Headers','Content-Type', 'Authorization') // Content-Type = contohnya json, (xml, html?) dll. // Authorization = berguna ketika proses pengiriman token kedalam API
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+ next(); // agar requestnya tidak berhenti sampai disitu
 })
 
 
