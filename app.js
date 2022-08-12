d10f9a0c700b825ff29d2dccc66fb6f571cda93d
EdsonSoumay <69837656+EdsonSoumay@users.noreply.github.com>
2022-08-12 09:17:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -9,15 +9,20 @@ const flash = require('connect-flash');
 var app = express();
 
 // untuk handle siapa sja yang dapat mengakses API kita
-app.use((req, res, next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*') // Origin = url yang ingin di berikan akses API 
-  res.setHeader('Access-Control-Allow-Methods','GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS') // method = method dalam penggunaan API 
-  res.setHeader('Access-Control-Allow-Headers','Content-Type = multipart/form-data  , Authorization') // Content-Type = contohnya json, (xml, html?) dll. // Authorization = berguna ketika proses pengiriman token kedalam API
-  //res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
- // res.setHeader('Access-Control-Allow-Headers', '*');
-///  res.setHeader('Content-Type', 'multipart/form-data');
- next(); // agar requestnya tidak berhenti sampai disitu
-})
+// app.use((req, res, next)=>{
+//   res.setHeader('Access-Control-Allow-Origin','*') // Origin = url yang ingin di berikan akses API 
+//   res.setHeader('Access-Control-Allow-Methods','GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS') // method = method dalam penggunaan API 
+//   res.setHeader('Access-Control-Allow-Headers','Content-Type = multipart/form-data  , Authorization') // Content-Type = contohnya json, (xml, html?) dll. // Authorization = berguna ketika proses pengiriman token kedalam API
+//   //res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//  // res.setHeader('Access-Control-Allow-Headers', '*');
+// ///  res.setHeader('Content-Type', 'multipart/form-data');
+//  next(); // agar requestnya tidak berhenti sampai disitu
+// })
+
+app.use((req, res, next) {
+    res.setHeader("Content-Type", "application/json");
+    next();
+});
 
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
