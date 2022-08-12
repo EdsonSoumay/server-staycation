8e67fba79d0107fa86affb7e8c62ca7d2c72666d
EdsonSoumay <69837656+EdsonSoumay@users.noreply.github.com>
2022-08-12 13:32:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ app.use(cors())
 //   next(); // agar requestnya tidak berhenti sampai disitu
 // })
 
-// app.use((req,res,next)=>{
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, Accept');
-//   next()
-// })
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT, PATCH');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, Accept');
+  next()
+})
 
 // app.use((req,res,next)=>{
 //   res.setHeader("Access-Control-Allow-Origin", '*');
